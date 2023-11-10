64924e1142547cf5b704e3824fcdf20b47810838
MeharwanW <108510260+MeharwanW@users.noreply.github.com>
2023-11-10 20:46:12 +0500

    All Files Added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Header.css"
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+    return (
+        <section>
+             <Nav className="justify-content-end h-menu" activeKey="/home">
+             <Button variant="light">Login</Button>{' '}
+        <Button variant="danger">Sign In</Button>{' '}
+      </Nav>
+     <Nav className="justify-content-end h-menu" activeKey="/home">
+         <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/venue">Venue</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/suppliers">Suppliers</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/Social Media">
+            Social Media 
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">
+            About
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+          <Nav.Link eventKey="/contact">
+           Contact Us 
+          </Nav.Link>
+        </Nav.Item> 
+      </Nav>
+     
+        </section>
+    );
+}
+
+export default Header;
