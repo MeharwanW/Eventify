3ad4432c4f87daa44fcfd016faaa7f76d4ca6886
MeharwanW <108510260+MeharwanW@users.noreply.github.com>
2024-03-25 02:18:20 +0500

    Modified Venue

--- src/components/Venues/Venue.jsx
+++ src/components/Venues/Venue.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Footer from '../Footer/Footer';
 import SearchVenue from '../SearchVenue/SearchVenue';
 import "./venue.css"
+import "swiper/css";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import data1 from "../../utils/slider1.json"
+import { sliderSettings } from "../../utils/common.js";
 
 export default function Venue() {
   return (
@@ -14,6 +18,28 @@ export default function Venue() {
           <div className='venueSearch'></div>
           <SearchVenue></SearchVenue>
         </div>
+      <div className='venueList'>
+      <div className="paddings innerWidth r-container ">
+        <div className="r-head flexCenter">
+          <span className="heading">Event Venues</span>
+        </div>
+        <div className='eventVenue'>
+          {data1.map((card) => (
+            <div className="flexColCenter r-card">
+                <img src={card.image} alt="home" />
+                <span className="flexCenter heading">{card.name}</span>
+                <span className="flexCenter font">{card.detail}</span>
+              </div>
+         
+         ))}
+        
+         </div>
+       
+      
+        
+      </div>
+      </div>
+
         <Footer></Footer>
     </div>
   )
