3bcfed9d34d3bf87d9110982118c59fb18a592f6
MeharwanW <108510260+MeharwanW@users.noreply.github.com>
2024-04-01 01:33:18 +0500

    Responsive All Website

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,35 +11,70 @@ import homeSvg1 from "../../assets/home.svg";
 import contactSvg1 from "../../assets/contact.svg";
 import mediaSvg1 from "../../assets/media.svg";
 import supplierSvg1 from "../../assets/supplier.svg";
+import { useState } from 'react';
 
 
 import React from 'react';
 
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  window.addEventListener('resize', () => {
+    setWindowWidth(window.innerWidth);
+  });
   return (
     <section>
-      <div className="justify-content-between navbar fixed-top navbar-scroll" activeKey="/home">
+      
+    
+      <div className="justify-content-between navbar fixed-top" activeKey="/home">
         <div className="">
           <Link to="Home" >
             <div className="Logo">
-              <img className="svgImg" src={Logo} alt="" srcset="" />
+              <img className="svgImg" src={Logo} alt="" srcSet="" />
             </div>
           </Link>
         </div>
-        <div className="d-flex linkSection justify-content-around">
-          <Nav.Item>
-            <Nav.Link className="links" eventKey="/Home">
-              <Link to="Home">
-                <div className="flexColCenter">
+        {!showMenu && windowWidth >= 953 && (<div className={`d-flex linkSection justify-content-around ${showMenu ? 'hide' : ''}`}>
+          <NavItems />
+        </div>
+        )}
+        <div className="buttons">
+          <Link to="/Login" className="font"><button className="button" variant="">
+            Login
+          </button></Link>
+          <Link to="SignUp" className="font link"><button className="button" variant="">
+            Sign Up
+          </button></Link>
+        </div>
+        
+      {windowWidth <953 && (<div className={`dropdown-menu ${showMenu ? 'show' : ''}`}>
+          <NavItems />
+        </div>)}
+        {windowWidth <953 &&(<div className="icon" onClick={() => setShowMenu(!showMenu)}>&#9776;</div>)}
+      </div>
+     
+      { <Outlet />}
+    </section>
+  );
+}
+
+function NavItems() {
+  return (
+    <>
+    
+      <Nav.Item>
+        <Nav.Link className="links" eventKey="/Home">
+          <Link to="Home">
+          <div className="flexColCenter">
                   <img className="svgImg flexColCenter" src={homeSvg1} alt="" srcset="" />
                   <div className="LinksInfo font">Home</div>
                 </div>
-              </Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
+          </Link>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
             <Nav.Link className="links" eventKey="/Venue">
               <Link to="Venues">
                 <div className="flexColCenter">
@@ -89,20 +124,8 @@ const Header = () => {
               </Link>
             </Nav.Link>
           </Nav.Item>
-        </div>
-        <div className="">
-          <Link to="/Login" className="font"><button className="button" variant="" >
-            Login
-          </button>{""}</Link>
-          <Link to="SignUp" className="font link"><button className="button" variant="">
-            Sign Up
-          </button>{""}</Link>
-        </div>
-      </div>
-      <Outlet />
-
-    </section>
+    </>
   );
-};
+}
 export default Header;
 
