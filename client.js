a81c46abefd632403c59c98e1076a019507d6dbf
meharwanK <meharwankumar.bscssef20@iba-suk.edu.pk>
2024-04-30 00:53:05 +0500

    Added By Meharwank Client and App.js

---
+++ client.js
@@ -0,0 +1,52 @@
+// const mongoose= require("mongoose")
+
+// mongoose.connect("mongodb+srv://eventify:ibasukkur@backenddb.vx1pj6l.mongodb.net/Eventify-Backend?retryWrites=true&w=majority&appName=BackendDB")
+// .then(()=>{
+//     console.log("mongodb connected");
+// })
+// .catch(()=>{
+//     console.log('failed');
+// })
+const mongoose = require("mongoose")
+
+
+const clientSchema=new mongoose.Schema({
+    client_name: {
+        type: "string",
+        required: [true,"Please enter your name"]
+    },
+    client_email:{
+        type:"string",
+        required: true,
+        unique:true
+    },
+    client_password:{
+        type: "string",
+        required: true
+        
+    },
+    client_cnic:{
+        type: "string",
+        required:true,
+        unique:true
+    },
+    client_username:{
+        type: "string",
+        required: [true,"Please enter user name"],
+        unique:true
+
+    },
+    client_phone:{
+        type:"string",
+        required: true,
+        unique:true
+        
+    }
+},
+{
+    timestamps:true
+})
+
+const client  = mongoose.model("clients",clientSchema)
+
+module.exports=client
