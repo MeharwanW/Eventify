696210b713a1a564696f4d3e6baf0bcaed23883d
MeharwanW <108510260+MeharwanW@users.noreply.github.com>
2024-05-06 13:32:30 +0500

    Booking changes

--- src/components/BookingEvent/Booking.jsx
+++ src/components/BookingEvent/Booking.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "./Booking.css";
+import SignatureCanvas from 'react-signature-canvas';
 
 export const Booking = () => {
+    const [sign,setSign] = useState()
+    const [url,setUrl] = useState()
+
+    const handleClear= () =>{
+        sign.clear()
+        setUrl('')
+    }
+    const handleGenerate= () =>{
+        setUrl(sign.getTrimmedCanvas().toDataURL('image/png'))
+    }
+
+    const [totalBudget, setTotalBudget] = useState(0);
     const [formData, setFormData] = useState({
         fullName: "",
         email: "",
@@ -11,11 +24,14 @@ export const Booking = () => {
         city: "",
         state: "",
         zipCode: "",
-        country: "",
+        venue:"",
         cameraman: false,
         decoration: false,
-        chairsPrize: 0,
         termsAgreed: false,
+        soundDj: false,
+        hall: false,
+        stage: false,
+        floor: false,
     });
     const [confirmation, setConfirmation] = useState("");
     const [errors, setErrors] = useState({});
@@ -27,6 +43,17 @@ export const Booking = () => {
             [name]: type === "checkbox" ? checked : value,
         });
     };
+    
+    useEffect(() => {
+        let budget = 0;
+        if (formData.cameraman) budget += 25000;
+        if (formData.decoration) budget += 25000;
+        if (formData.soundDj) budget += 25000;
+        if (formData.hall) budget += 25000;
+        if (formData.stage) budget += 25000;
+        if (formData.floor) budget += 25000;
+        setTotalBudget(budget);
+    }, [formData]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -39,29 +66,33 @@ export const Booking = () => {
         });
 
         if (Object.keys(formErrors).length === 0) {
-            // Calculate total prize
-            let totalPrize = 0;
-            if (formData.cameraman) totalPrize += 25000;
-            if (formData.decoration) totalPrize += 40000;
-            totalPrize += parseInt(formData.chairsPrize);
+            
+            let budget = 0;
+            if (formData.cameraman) budget += 25000;
+        if (formData.decoration) budget += 25000;
+        if (formData.soundDj) budget += 25000;
+        if (formData.hall) budget += 25000;
+        if (formData.stage) budget += 25000;
+        if (formData.floor) budget += 25000;
+            budget += parseInt(formData.chairsPrize);
+            setTotalBudget(budget);
 
             // Form submission logic here
-            setConfirmation(`Thank you, ${formData.fullName}! Your event on ${formData.eventDate} has been booked. Total prize: ${totalPrize}`);
+            setConfirmation(`Thank you, ${formData.fullName}! Your event on ${formData.eventDate} has been booked. Total prize: ${budget}`);
         } else {
             setErrors(formErrors);
         }
     };
 
     return (
-        <div className="flexCenter font">
-            <form onSubmit={handleSubmit} className="headers">
+        <div className="bookingscreen shadow-box flexCenter font">
+            <form onSubmit={handleSubmit} className="bookingagreement ">
                 <h1 className="text">Booking Agreement</h1>
                 <p>Complete form below to retain the services of an Event</p>
-                <br />
                 <div>For Direct Inquiries contact Meharwanw@gmail.com or call (03488365045)</div>
                 <br />
-                <label htmlFor="event">Event:</label>
-                <select id="event" name="event" value={formData.event} onChange={handleChange} required>
+                <label className="text flexCenter" htmlFor="event ">Event:</label>
+                <select id="event" className="input" name="event" value={formData.event} onChange={handleChange} required>
                     <option value="">Select Event</option>
                     <option value="Wedding">Wedding</option>
                     <option value="Meeting">Meeting</option>
@@ -70,48 +101,119 @@ export const Booking = () => {
                 </select>
                 {errors.event && <div className="error">{errors.event}</div>}
                 <br />
-                <label htmlFor="eventDate">Event Date & Time:</label>
-                <input type="date" id="eventDate" name="eventDate" value={formData.eventDate} onChange={handleChange} required />
-                <input type="time" id="eventTime" name="eventTime" value={formData.eventTime} onChange={handleChange} required />
+                <label className="text" htmlFor="eventDate">Event Date & Time:</label>
+                <div className="input1">
+                    <input type="date" id="eventDate" name="eventDate" value={formData.eventDate} onChange={handleChange} required />
+                    <input type="time" id="eventTime" name="eventTime" value={formData.eventTime} onChange={handleChange} required />
+                </div>
                 {errors.eventDate && <div className="error">{errors.eventDate}</div>}
                 {errors.eventTime && <div className="error">{errors.eventTime}</div>}
                 <br />
-                <label htmlFor="city">City:</label>
-                <input type="text" id="city" name="city" value={formData.city} onChange={handleChange} required />
-                {errors.city && <div className="error">{errors.city}</div>}
-                <label htmlFor="state">State:</label>
-                <input type="text" id="state" name="state" value={formData.state} onChange={handleChange} required />
-                {errors.state && <div className="error">{errors.state}</div>}
-                <label htmlFor="zipCode">Zip Code:</label>
-                <input type="text" id="zipCode" name="zipCode" value={formData.zipCode} onChange={handleChange} required />
-                {errors.zipCode && <div className="error">{errors.zipCode}</div>}
+                <div className="inputs flexCenter Address">
+                <label className="text" htmlFor="eventDate">Address</label>
+             
+                <input type="text" id="venue" name="venue" placeholder="Venue" className="font input1" value={formData.venue} onChange={handleChange} required />
+                    {errors.venue && <div className="error">{errors.venue}</div>}
+                    <input type="text" id="city" placeholder="City" className="font input1" name="city" value={formData.city} onChange={handleChange} required />
+                    {errors.city && <div className="error">{errors.city}</div>}
+                    <input type="text" id="state" name="state" placeholder="State" className="font input1" value={formData.state} onChange={handleChange} required />
+                    {errors.state && <div className="error">{errors.state}</div>}
+                    <input type="text" id="zipCode" name="zipCode" placeholder="ZipCode" className="font input1" value={formData.zipCode} onChange={handleChange} required />
+                    {errors.zipCode && <div className="error">{errors.zipCode}</div>}
+                </div>
                 <br />
-                <label>
-                    <input type="checkbox" name="cameraman" checked={formData.cameraman} onChange={handleChange} />
-                    Cameraman (+$25,000)
-                </label>
-                <br />
-                <label>
-                    <input type="checkbox" name="decoration" checked={formData.decoration} onChange={handleChange} />
-                    Decoration (+$40,000)
-                </label>
-                <br />
-                <label htmlFor="chairsPrize">Chairs Prize:</label>
-                <input type="number" id="chairsPrize" name="chairsPrize" value={formData.chairsPrize} onChange={handleChange} />
-                <br />
-                {/* Terms of Agreement */}
-                <h4>Terms of Agreement</h4>
-                <label>
-                    <input type="checkbox" name="termsAgreed" checked={formData.termsAgreed} onChange={handleChange} required />
-                    I agree to the terms and conditions
-                </label>
-                {errors.termsAgreed && <div className="error">{errors.termsAgreed}</div>}
-                <br />
-                <label>Date</label>
-                <input type="text" value="05-01-2024" readOnly />
-                <br />
-                <label>Pick a Date</label>
-                <input type="date" />
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <input type="checkbox" id="cameraman" name="cameraman" checked={formData.cameraman} onChange={handleChange} />
+                        <span class="checkbox-icon"></span>
+                        Cameraman
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">(+25,000)</span>
+                    </div>
+                </div>
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <input type="checkbox" id="decoration" name="decoration" checked={formData.decoration} onChange={handleChange} />
+                        <span class="checkbox-icon"></span>
+                        Decoration
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">(+25,000)</span>
+                    </div>
+                </div>
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <input type="checkbox" id="soundDj" name="soundDj" checked={formData.soundDj} onChange={handleChange} />
+                        <span class="checkbox-icon"></span>
+                        Sound DJ
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">(+25,000)</span>
+                    </div>
+                </div>
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <input type="checkbox" id="hall" name="hall" checked={formData.hall} onChange={handleChange} />
+                        <span class="checkbox-icon"></span>
+                        Hall
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">(+25,000)</span>
+                    </div>
+                </div>
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <input type="checkbox" id="stage" name="stage" checked={formData.stage} onChange={handleChange} />
+                        <span class="checkbox-icon"></span>
+                        Stage
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">(+25,000)</span>
+                    </div>
+                </div>
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <input type="checkbox" id="floor" name="floor" checked={formData.floor} onChange={handleChange} />
+                        <span class="checkbox-icon"></span>
+                        DJ Floor
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">(+25,000)</span>
+                    </div>
+                </div>
+                <div class="flexCenter input1 budgetEvent">
+                    <div class="budgetCheckbox flexCenter">
+                        <label type="label" id="total" name="total" />
+                        <span class="checkbox-icon"></span>
+                       Total
+                    </div>
+                    <div class="budgetBox">
+                        <span class="product">{totalBudget}</span>
+                    </div>
+                </div>
+
+            <br />
+               
+               <div className="flexColCenter">
+               <label className="text flexCenter" htmlFor="">Signature</label>
+               <div className="Signature">
+
+            
+                <SignatureCanvas 
+                    canvasProps={{width: 200, height: 200, className: 'sigCanvas'}}
+                    ref={data=>setSign(data)}
+                    />
+
+                    </div>
+            <br/>
+            <div className="flexCenter SignatureButtons">
+            <button className="button" onClick={handleClear}>Clear</button>
+            <button className="button" onClick={handleGenerate}>Save</button>
+            </div>
+            <br/>
+                    </div>
+              
                 <br />
                 <button className="button" type="submit">Submit</button>
             </form>
